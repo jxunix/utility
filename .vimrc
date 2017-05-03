7f1941b7b78ce0069431461f9dde132895b29115
Jun Xu <xnirvana508@gmail.com>
2017-05-02 23:02:16 -0400

    update

--- .vimrc
+++ .vimrc
@@ -14,8 +14,6 @@ set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
 Bundle 'ervandew/supertab'
 Plugin 'gmarik/Vundle.vim'
-Plugin 'majutsushi/tagbar'
-Plugin 'scrooloose/nerdtree'
 Plugin 'scrooloose/nerdcommenter'
 Plugin 'SirVer/ultisnips'
 Plugin 'honza/vim-snippets'
--- darwin/install.sh
+++ darwin/install.sh
@@ -2,38 +2,12 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install homebrew/python/numpy
-brew install matplotlib
-brew install tmux
-
-brew cask install citrix-receiver
-brew cask install dash
 brew cask install dropbox
 brew cask install flash
 brew cask install google-chrome
 brew cask install iterm2
-brew cask install mactex
 brew cask install macvim
-brew cask install moom
-brew cask install skype
 
 brew tap homebrew/science
 brew install r
 brew install Caskroom/cask/rstudio
-
-ssh-keygen -t rsa -C "xnirvana508@gmail.com"
-# copy ssh key to github.com
-vim ~/.ssh/id_rsa.pub
-ssh -T git@github.com
-
-if [ ! -d "~/.vim" ]; then
-	mkdir ~/.vim
-fi
-cp -R ftplugin ~/.vim
-cp -R template ~/.vim
-
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.py --clang-completer
