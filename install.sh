06508cbdf283b0aacbfb280aa0012ca81c6edded
Jun Xu <xnirvana508@gmail.com>
2017-07-29 23:00:30 -0400

    update

--- install.sh
+++ install.sh
@@ -1,78 +1,101 @@
 #!/bin/bash
 
-sudo -i
+echo "+-----------------------+"
+echo "| #1. nvidia gpu driver |"
+echo "| #2. git               |"
+echo "| #3. everything else   |"
+echo "+-----------------------+"
 
-apt update -y
-apt dist-upgrade -y
+read -p "Which to install? " option
+case $option in
+    [1]* )
 
-add-apt-repository ppa:graphics-drivers/ppa
+        version=$(lsb_release -a 2>/dev/null | \
+            grep Description | \
+            sed -r 's/.* ([0-9]*\.[0-9]*\.[0-9]*) .*/\1/')
+        nvidia=$(apt search nvidia 2>/dev/null | \
+            grep -E "nvidia-[0-9]{3}/.*$version" | \
+            tail -1 | \
+            cut -d '/' -f 1)
 
-apt install -y ibus
-apt install -y ibus-clutter
-apt install -y ibus-gtk
-apt install -y ibus-gtk3
-apt install -y ibus-qt4
-apt install -y ibus-pinyin
-ibus restart
+        sudo add-apt-repository ppa:graphics-drivers/ppa
+        sudo apt update  -y
+        sudo apt install -y $nvidia
+        sudo reboot
 
-apt install -y build-essential
-apt install -y cmake
-apt install -y ctags
-apt install -y default-jdk
-apt install -y fcitx-googlepinyin
-apt install -y git
-apt install -y nautilus-dropbox
-apt install -y python-dev
-apt install -y python-numpy
-apt install -y python3-numpy
-apt install -y r-base
-apt install -y texlive-full
-apt install -y unrar
-apt install -y vim
+        ;;
 
-apt update -y
-apt dist-upgrade -y
+    [2]* )
 
-exit
+        sudo apt install -y vim
+        sudo apt install -y git
+        ssh-keygen -t rsa -C "xnirvana508@gmail.com"
+        cat ~/.ssh/id_rsa.pub
+        while true; do
+            read -p "Have you copied the public key to github.com? " yn
+            case $yn in
+                [Yy]* ) ssh -T git@github.com; break;;
+                [Nn]* ) echo "Please do it.";;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
 
-# ssh to github
-ssh-keygen -t rsa -C "xnirvana508@gmail.com"
-cat ~/.ssh/id_rsa.pub
-while true; do
-    read -p "Have you copied the public key to github.com? " yn
-    case $yn in
-        [Yy]* ) ssh -T git@github.com; break;;
-        [Nn]* ) echo "Please do it.";;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+        dir_utility="/home/jun/Documents/code/utility"
+        git_utility="https://github.com/jxunix/utility.git"
 
-# .vimrc and .vim
-dir_utility="/home/jun/Documents/utility"
-git_utility="https://github.com/jxunix/utility.git"
+        git clone "$git_utility" "$dir_utility"
+        ln -sf  "$dir_utility"/.bashrc    ~/.bashrc
+        ln -sf  "$dir_utility"/.vimrc     ~/.vimrc
+        ln -sf  "$dir_utility"/.gitconfig ~/.gitconfig
+        ln -sfn "$dir_utility"/colors     ~/.vim/colors
+        ln -sfn "$dir_utility"/ftplugin   ~/.vim/ftplugin
 
-git clone "$git_utility" "$dir_utility"
-ln -sf "$dir_utility"/.vimrc    ~/.vimrc
-ln -sf "$dir_utility"/colors    ~/.vim/colors
-ln -sf "$dir_utility"/ftplugin  ~/.vim/ftplugin
+        dir_vundle="/home/jun/.vim/bundle/Vundle.vim"
+        git_vundle="https://github.com/gmarik/Vundle.vim.git" 
 
-# .bashrc and .gitconfig
-ln -sf "dir_utility"/.bashrc    ~/.bashrc
-ln -sf "dir_utility"/.gitconfig ~/.gitconfig
+        git clone "$git_vundle" "$dir_vundle"
 
-# vundle
-dir_vundle="/home/jun/.vim/bundle/Vundle.vim"
-git_vundle="https://github.com/gmarik/Vundle.vim.git" 
+        dir_ycm="/home/jun/.vim/bundle/YouCompleteMe"
+        git_ycm="https://github.com/Valloric/YouCompleteMe"
 
-git clone "$git_vundle" "$dir_vundle"
+        git clone "$git_ycm" "$dir_ycm"
+        cd "$dir_ycm"
+        git submodule update --init --recursive
+        ./install.py --clang-completer
+        cd -
 
-# youcompleteme
-dir_ycm="/home/jun/.vim/bundle/YouCompleteMe"
-git_ycm="https://github.com/Valloric/YouCompleteMe"
+        ;;
 
-git clone "$git_ycm" "$dir_ycm"
-cd "$dir_ycm"
-git submodule update --init --recursive
-./install.py --clang-completer
+    [3]* )
 
-cd
+        sudo -i
+
+        apt install -y build-essential
+        apt install -y cmake
+        apt install -y ctags
+        apt install -y default-jdk
+        apt install -y git
+        apt install -y ibus-pinyin
+        apt install -y nautilus-dropbox
+        apt install -y python-dev
+        apt install -y python-numpy
+        apt install -y python3-numpy
+        apt install -y r-base
+        apt install -y texlive-full
+        apt install -y unrar
+
+        ibus restart
+
+        apt update -y
+        apt dist-upgrade -y
+
+        exit
+
+        ;;
+
+    * )
+
+        echo "Please choose one of the listed options."
+        ;;
+
+esac
